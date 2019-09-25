c9d5466d53c0ad5432d49c19bc2e4bb5975d9501
v18011792 <alexis.vatin@etu.univ-amu.fr>
2019-09-25 08:48:55 +0200

    création projet + td1

--- tp2.php
+++ tp2.php
@@ -1,21 +1,21 @@
 <?php
 <!DOCTYPE html>
 <html>
-<head>
-</head>
-    <body>
-        <?php
-            echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
-            echo 'C\'est pas mal non ? <br/>';
-            $var1 = 6;
-            $var2 = 1.3;
-            $var3 = 'Variable 3';
-            ?>
-
-        <?php echo "$var1 + $var2"; ?>
-        <?php echo $var1 + $var2; ?>
-        <?php echo $var1 + $var3; ?>
-    </body>
-
-    </html>
+<head></head>
+<body>
+<?php
+echo '<strong> Voici mon premier programme PHP </strong><br/>, "\n"';
+echo 'c\'est pas mal non ? <br/>';
+&var1 = 6;
+&var2 = 1.3;
+&var3 = 'Variable 3';
 ?>
+<?php echo "&var1 + &var2"; ?>
+<?php echo &var1 + &var2; ?>
+<?php echo &var1 + &var3; ?>
+
+</body>
+</html>
+
+
+
